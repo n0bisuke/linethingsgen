1485e1946cf9a13f8acc43eca5143c535d6262cf
のびすけ <rsksound@gmail.com>
2019-06-06 02:01:18 +0900

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,11 +51,11 @@ export default {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    proxy: true,
+    // proxy: true,
   },
   proxy: {
-    '/connpass': 'https://connpass.com/api/v1/event/?keyword=python',
-    '/thingsproducts': `https://api.line.me/things/v1/trial/products`
+    // '/things': 'https://api.line.me/things/v1/trial/products',
+    // '/connpass': 'https://connpass.com/api/v1/event/?keyword=python'
   },
 
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,25 @@
         <b-input v-model="accesstoken"></b-input>
       </b-field>
       
-      <b-field label="liffID">
+      <b-field label="liffId">
         <b-input v-model="liffId"></b-input>
       </b-field>
+
+      <b-field label="Name">
+        <b-input v-model="name"></b-input>
+      </b-field>
+
+      <b-field label="ProductId">
+        <b-input v-model="productId"></b-input>
+      </b-field>
+
+      <b-field label="serviceUuid">
+        <b-input v-model="serviceUuid"></b-input>
+      </b-field>
       
-      <b-button type="is-success" @click="getProducs">Success</b-button>
+      <b-button type="is-success" @click="getProducs">確認</b-button>
+      <b-button type="is-success" @click="createProducs">作成</b-button>
+      <b-button type="is-success" @click="setScenarioset">シナリオセット登録</b-button>
 
     </section>
 
@@ -27,7 +41,11 @@ export default {
   data () {
     return {
       accesstoken: '',
-      liffId: ''
+      liffId: '',
+      name: '',
+      baseUrl: 'https://ev2-prod-node-red-fe7bce69-69d.herokuapp.com',
+      productId: '',
+      serviceUuid: ''
     }
   },
 
@@ -36,28 +54,78 @@ export default {
   },
 
   methods: {
-    getProducs: async function(){
-      console.log('click!')
-      const config = {
-        baseURL: '/',
-        url: '/thingsproducts',
-        method: 'GET',
+    //axiosのconfigを作成
+    _createConf: function(path, method, data={}){
+      return {
+        baseURL: this.baseUrl,
+        url: `/?url=${path}`,
+        method: method,
         headers: {
           Authorization: `Bearer ${this.accesstoken}`,
-        }
+        },
+        data: data
       };
-      
+    },
+
+    //
+    getProducs: async function(){
+      console.log('click!');
+      const path = `https://api.line.me/things/v1/trial/products`
+      const config = this._createConf(path,'GET');
       try {
         const res = await axios.request(config);
         console.log(res.data);
       } catch (error) {
         console.log(error)
       }
+    },
+
+    //プロダクト作成
+    createProducs: async function(){
+      console.log('click!');
+      const path = `https://api.line.me/things/v1/trial/products`
+      const data = {
+        name: this.name,
+        liffId: this.liffId
+      };
+      const config = this._createConf(path,'POST',data);
+      try {
+        const res = await axios.request(config);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error)
+      }
+    },
+
+    //シナリオセット作成
+    setScenarioset: async function() {
+      console.log('click!');
+      const path = `https://api.line.me/things/v1/products/${this.productId}/scenario-set`;
+      const data = {
+        autoClose: false,
+        suppressionInterval: 0,
+        scenarios: [{
+          trigger: {
+            type: "BLE_NOTIFICATION",
+            serviceUuid: this.serviceUuid,
+            characteristicUuid: "62FBD229-6EDD-4D1A-B554-5C4E1BB29169"
+          },
+          actions: []
+        }],
+        productId: `${this.productId}`
+      };
+      const config = this._createConf(path,'PUT',data);
+      try {
+        const res = await axios.request(config);
+        console.log(res.data);
+        console.log(JSON.stringify(res.data));
+      } catch (error) {
+        console.log(error)
+      }
     }
+
   },
 
-  mounted: async () =>{
-
-  }
+  mounted: async () =>{}
 }
 </script>
